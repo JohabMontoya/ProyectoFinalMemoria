827e4372dc71a421890ab9dfabc4fe566a063852
johab <johab.montoya@uabc.edu.mx>
2025-11-14 22:59:31 -0800

    Clase jugador

--- src/main/java/ClasesMemoria/Jugador.java
+++ src/main/java/ClasesMemoria/Jugador.java
@@ -1,4 +1,30 @@
 package ClasesMemoria;
 
 public class Jugador {
+
+    private String nombre;
+    private int puntos;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.puntos = 0;  // inicia sin puntos
+    }
+
+    // Suma un punto cuando encuentra un par
+    public void sumarPunto() {
+        puntos++;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (Puntos: " + puntos + ")";
+    }
 }
